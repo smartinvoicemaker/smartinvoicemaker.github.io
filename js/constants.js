4c6e68ae5fb8e7c63de07f266e9ef0f7840abbc7
minhlv <dinhminh512@gmail.com>
2025-03-09 10:12:51 +0700

    feat: add constants file for shared links and update HTML to use dynamic URLs

---
+++ js/constants.js
@@ -0,0 +1,23 @@
+// Constants for shared links across the website
+const LINKS = {
+  // Main website URL
+  WEBSITE_URL: 'https://ez-invoice.com/',
+  
+  // App download links
+  ANDROID_APP: 'https://play.google.com/store/apps/details?id=invoice.invoicemaker.simple',
+  IOS_APP: 'https://apps.apple.com/app/ezinvoice-easy-invoice-maker/id6478380167',
+  
+  // Page links
+  TERMS_PAGE: 'pages/terms.html',
+  PRIVACY_PAGE: 'pages/privacy.html',
+  INVOICE_APP_PAGE: 'pages/best-invoice-app.html',
+  INVOICE_RECEIPT_PAGE: 'pages/invoice-recipt.html',
+  
+  // Image paths
+  LOGO: './images/logo.png',
+  GOOGLE_PLAY_BADGE: './images/goolge-play.svg',
+  APPSTORE_BADGE: './images/apple-store.svg'
+};
+
+// Make constants available globally
+window.APP_CONSTANTS = LINKS;
